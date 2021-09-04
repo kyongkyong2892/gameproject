a85afefffa55d645f37cd61121ebc912b216b5a5
pupungeggang <kyongkyong289@icloud.com>
2021-09-04 17:16:32 +0900

    Improved

--- jsdefense/Map.js
+++ jsdefense/Map.js
@@ -3,5 +3,10 @@ function manageMap() {
 }
 
 function displayMap() {
+    context.font = '60px Verdana';
+    context.linewWidth = 2;
+    context.fillStyle = 'black';
+    context.strokeStyle = 'black';
+    
     context.clearRect(UI.full[0], UI.full[1], UI.full[2], UI.full[3]);
 }
