4ab0125fb9a93c5d16f6857e1f2733b102a6319a
pupungeggang <kyongkyong289@icloud.com>
2021-09-06 13:05:20 +0900

    Improved

---
+++ jsdungeon1/CardData.js
@@ -0,0 +1,7 @@
+var cardData = [
+    {'ID' : 10001, 'Type' : 'Unit', 'Element' : 'Neutral', 'Rarity' : 'Basic', 'Name' : 'Cake 1', 'Energy' : 1, 'Stat' : [1, 2], 'Special' : '', 'Play' : ['U', 'E', 'E']},
+    {'ID' : 10002, 'Type' : 'Unit', 'Element' : 'Neutral', 'Rarity' : 'Basic', 'Name' : 'Cake 2', 'Energy' : 2, 'Stat' : [1, 2], 'Special' : '', 'Play' : ['U', 'E', 'E']},
+    {'ID' : 10003, 'Type' : 'Unit', 'Element' : 'Neutral', 'Rarity' : 'Basic', 'Name' : 'Cake 3', 'Energy' : 3, 'Stat' : [1, 2], 'Special' : '', 'Play' : ['U', 'E', 'E']},
+    {'ID' : 10004, 'Type' : 'Unit', 'Element' : 'Neutral', 'Rarity' : 'Basic', 'Name' : 'Cake 1', 'Energy' : 1, 'Stat' : [1, 2], 'Special' : '', 'Play' : ['U', 'E', 'E']},
+    {'ID' : 10005, 'Type' : 'Unit', 'Element' : 'Neutral', 'Rarity' : 'Basic', 'Name' : 'Cake 1', 'Energy' : 1, 'Stat' : [1, 2], 'Special' : '', 'Play' : ['U', 'E', 'E']},
+];
